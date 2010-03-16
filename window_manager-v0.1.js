11644ebb08c961a4b19de05144c8f26987189541
thecoshman <thecoshman@12b62c56-303f-11df-9f25-831aa717a150>
2010-03-16 13:58:57 +0000

    Initial update, starting at v0.1 (removed surplus files)

---
+++ window_manager-v0.1.js
@@ -0,0 +1,96 @@
+/*
+Copyright 2010 Dan Cosh
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+function Window_manager(){
+  this.windows = [];
+  this.menu_bar = null;
+  this.cursor_start_x = null;
+  this.cursor_start_y = null;
+  
+  this.start_menu_bar = function(){
+    if(this.menu_bar != null)
+    {
+      return;
+    }
+    var tbody = document.getElementsByTagName("body")[0];
+    this.menu_bar = document.createElement('div');
+    this.menu_bar.className = "menu_bar";
+    tbody.appendChild(this.menu_bar);
+  }
+  this.new_window = function(){
+    this.windows[this.windows.length] = new Window(this.windows.length);
+    return this.windows[this.windows.length - 1];
+  }
+  this.get_top_window = function(){
+    if(this.windows.length == 0)
+    {
+      return false;
+    }
+    var top_window = this.windows[0];
+    for(var i = 0; i < this.windows.length; i = i + 1)
+    {
+      if(this.windows[i].get_zindex() > top_window.get_zindex())
+      {
+        top_window = this.windows[i];
+      }
+    }
+    return top_window;
+  }
+  this.is_zindex_free = function(test_zindex){
+    for(var i = 0; i < this.windows.length; i = i + 1)
+    {
+      if(this.windows[i].get_zindex() == test_zindex)
+      {
+        return false;
+      }
+    }
+    return true;
+  }
+  this.get_cursor_x = function(){
+    if (browser.isIE){
+      return window.event.clientX + document.documentElement.scrollLeft + document.body.scrollLeft;
+    }
+    if (browser.isNS){
+      return event.clientX + window.scrollX;
+    }
+  }
+  this.get_cursor_y = function(){
+    if (browser.isIE){
+      return window.event.clientY + document.documentElement.scrollTop  + document.body.scrollTop;
+    }
+    if (browser.isNS){
+      return event.clientY + window.scrollY;
+    }
+  }
+  this.store_current_mouse = function(){
+    if (browser.isIE){
+      this.cursor_start_x = window.event.clientX + document.documentElement.scrollLeft + document.body.scrollLeft;
+      this.cursor_start_y = window.event.clientY + document.documentElement.scrollTop  + document.body.scrollTop;
+    }
+    if (browser.isNS){
+      this.cursor_start_x = event.clientX + window.scrollX;
+      this.cursor_start_y = event.clientY + window.scrollY;
+    }
+  }
+  this.get_start_mouse_x = function(){
+    return this.cursor_start_x;
+  }
+  this.get_start_mouse_y = function(){
+    return this.cursor_start_y;
+  }
+  return true;
+}
+var Window_manager = new Window_manager();
